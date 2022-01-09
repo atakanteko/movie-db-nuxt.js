2d3ab740ddd91259e2808f066a96efe946d70503
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-01-09 13:37:13 +0300

    add router for single movie

---
+++ pages/movies/_id/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <SingleMovie />
+  </div>
+</template>
+
+<script>
+import SingleMovie from '~/components/SingleMovie';
+
+export default {
+  components: {
+    SingleMovie,
+  },
+};
+</script>
